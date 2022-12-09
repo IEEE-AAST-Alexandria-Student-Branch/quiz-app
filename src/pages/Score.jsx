8b0f464bf2c86ed698ed16d9e03302506f925a08
Abdelrahman Essawy <boda.m7amd@gmail.com>
2022-12-09 06:10:31 +0200

    cleaning questionsstore before play again

--- src/pages/Score.jsx
+++ src/pages/Score.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import PrimaryButton from '../components/Form/PrimaryButton';
 import { Link } from 'react-router-dom';
+import { clearQuestions } from '../features/questions/questionsSlice';
 
 export default function Score() {
     const { score, displayName } = useSelector(store => store.user)
-
+    const dispatch = useDispatch()
+    const handleClick = () => {
+        dispatch(clearQuestions())  
+    }
+    
     return (
         <div>
             <img src='lamp.png' />
@@ -13,8 +18,8 @@ export default function Score() {
             <h1 style={{ color: 'white', fontSize: '2rem' }}>Congrats {displayName}! Your Score is {score / 10 * 100}%</h1>
             <div style={{ gap: '5rem', display: 'flex' }}>
                 <PrimaryButton content={'View my answers'} />
-                <Link to={"/start"}>
-                <PrimaryButton content={'Play Again'} />
+                <Link onClick={handleClick} to={"/start"}>
+                    <PrimaryButton content={'Play Again'} />
 
                 </Link>
 
